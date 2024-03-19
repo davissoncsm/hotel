0d94b86c40b4193a3db69e569b58ffb42288e4b8
davissoncsm <davisson.charles@gmail.com>
2024-03-19 10:06:07 -0300

    Define entities

---
+++ app/Entities/HotelEntity.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entities;
+
+use Database\Factories\HotelFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class HotelEntity extends Model
+{
+    use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    public $table = 'hotels';
+
+    /** The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'address',
+        'city',
+        'state',
+        'zip_code',
+        'website',
+    ];
+
+    protected static function newFactory(): Factory
+    {
+        return HotelFactory::new();
+    }
+}
---
+++ app/Entities/RoomEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entities;
+
+use Database\Factories\RoomFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RoomEntity extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    public $table = 'rooms';
+
+    /** The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'hotel_id',
+        'name',
+        'description',
+    ];
+
+    protected static function newFactory(): Factory
+    {
+        return RoomFactory::new();
+    }
+}
