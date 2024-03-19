33b75e2d294171d875045e7dd90275eb431ba78e
davissoncsm <davisson.charles@gmail.com>
2024-03-19 10:07:14 -0300

    Define migrations and seeders

---
+++ database/migrations/2024_03_18_202333_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('hotel_id');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->foreign('hotel_id')
+                ->references('id')
+                ->on('hotels')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
